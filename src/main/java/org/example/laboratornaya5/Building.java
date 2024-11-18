f17f979b9235666c6909410129c2d6d3c4a4d76c
dania <daniarmendygaliev14@gmail.com>
2024-11-18 09:34:29 +0500

    готово

--- src/main/java/org/example/laboratornaya5/Building.java
+++ src/main/java/org/example/laboratornaya5/Building.java
@@ -57,7 +57,7 @@ class Wooden extends Building {
 
     @Override
     public void update(long time) {
-        // Реализация логики обновления для деревянного дома
+        // Реализация логики обновления kjдля деревянного дома
     }
 
     @Override
